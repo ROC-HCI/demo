79ef1fea530b31558f4c5bf50eba8b5e2124563e
abbepiels <40579741+abbepiels@users.noreply.github.com>
2021-03-09 11:19:42 -0500

    Update results_buffer.js

--- src/pages/results_buffer.js
+++ src/pages/results_buffer.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import Link from 'react-router-dom/Link'
+import Lottie from 'react-lottie'
+import load from '../lotties/loading';
 
 class results_buffers extends Component {
     render() {
+        const defaultOptions = {
+            loop: true,
+            autoplay: true,
+            animationData: load,
+            rendererSettings: {
+                preserveAspectRation: "xMidYMid slice"
+            }
+        };
         return (
-            <div>
-                <h1>YOUR RESULTS ARE LOADING PAGE</h1>
-                <Button color='inherit' component={Link} to="/results">
+            <div style = {{display: 'flex', flexDirection: 'column', backgroundColor: 'white', height:'100vh'}}>
+                <div style = {{textAlign: 'center'}}>
+                <h1 style = {{color: 'gray'}}>YOUR RESULTS ARE LOADING PAGE</h1></div>
+                <Lottie 
+                    options = {defaultOptions}
+                    height = {400}
+                    width = {400} 
+                    />
+                    
+                <Button style = {{color: 'gray', component:{Link}, to: "/results", fontSize: '3vh'}}>
     To Results
-  </Button>
+  </Button><div>
+            </div>
             </div>
         )
     }
