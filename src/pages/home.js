a751d4c7ac066d6b9f218209cdad2a26b659e56e
abbepiels <40579741+abbepiels@users.noreply.github.com>
2021-03-09 00:11:22 -0500

    Update home.js

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,17 +1,63 @@
 import React, { Component } from 'react'
 import Button from '@material-ui/core/Button'
 import Link from 'react-router-dom/Link'
+import Lottie from 'react-lottie'
+import "./home.css";
+import ParkHomepageImage from "./ParkHomepageImage.png"
 
 class home extends Component {
+
     render() {
+        const StyledButton = {
+            fontSize: '3vh',
+            color: '#0261FF'
+        };
         return (
-            <div>
-                <h1>HOME PAGE</h1>
-                <Button color='inherit' component={Link} to="/task_tutorial">
-    To Task Tutorial
-  </Button>
+            
+            <div style={{ display: 'flex', backgroundColor: 'white', height: '20vh', flexDirection: 'column' }}>
+                <div style={{ textAlign: 'left', flexDirection: 'row' }}>
+                    <h1 style={{ color: '#0261FF', fontSize: '8vh' }} > PARK
+                    <Button style={{ color: '#0261FF', component: { Link }, to: "/home", fontSize: '2vh' }}>
+                            <b>Home</b>
+                        </Button>
+                        <Button style={{ color: '#0261FF', component: { Link }, to: "/About", fontSize: '2vh' }}>
+                            <b>About</b>
+                        </Button>
+                        <Button style={{ color: '#0261FF', component: { Link }, to: "/FAQ", fontSize: '2vh' }}>
+                            <b>FAQ</b>
+                        </Button>
+                        <Button style={{ color: '#0261FF', component: { Link }, to: "/Contact", fontSize: '2vh' }}>
+                            <b>Contact</b>
+                        </Button>  </h1></div>
+
+                <div style={{ display: 'flex', flexDirection: 'column', height: '90vh', background: 'linear-gradient(to right, #0161FC, #529E36)' }} >
+                    <h2 style={{ color: 'white', fontSize: '5.5vh' }}> The PARK system enables  <br /> the measurement of  <br /> Parkinson's disease for <br />
+                        <em>anyone, anywhere -</em> <br />- via webcam</h2>
+                    <br></br>
+                    <div style = {{display: 'flex', flexDirection: 'row'}}>
+                    <div style = {{display: 'flex', flexDirection: 'column'}}>
+                    <button id="startStudy"
+                        className="btn btn-circle btn-xl" >
+                        Start Study
+                  </button>
+                    <br />
+                    <br />
+                    
+                    <h3 style = {{color: 'white'}}>
+                        We ask that only those with Parkinson's
+                        complete the study at this time.
+                     </h3>
+                    </div>
+                    <h4 style = {{alignitems: 'right'}}><img className= "photo" src={ParkHomepageImage}></img></h4>
+                </div>
+                </div>
+                <div>
+               
+                </div>
             </div>
+
         )
+
     }
 }
 
